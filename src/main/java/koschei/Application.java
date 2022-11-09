ba143959c6e44b5022af949a0dc3f386c206a567
mvlad17 <mvladwork@gmail.com>
2022-11-09 07:10:10 +0500

    task solution 2.1.5

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -1,15 +1,20 @@
 package koschei;
 
 import koschei.config.AppConfig;
+import koschei.models.Deth8;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
+
         System.out.println(koscheiTheDeathless.getRulesByDeth());
+
     }
 }
